b057dfd11db7dadd5c8876565d2c2870d9abd0ba
billk97 <vasileios@konstantinou.dev>
2021-07-24 02:31:45 +0300

    add a logo to my persona page

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,6 @@
 <template>
     <b-navbar toggleable="lg" type="dark" class="nav-bar">
+        <img src="@/assets/logo.png" alt="my logo image" style="margin-left: 10px;">
         <b-navbar-brand href="/">
             <p id="fullName" ref="fullName" style="margin-left: 10px; min-width: 100px; margin-top: 11px;"></p>
         </b-navbar-brand>
